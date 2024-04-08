bbfc57036eb750563820a442e151b75b0dc25b37
Henzz <khenok53@gmail.com>
2024-04-08 04:27:44 +0300

    task 3 done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Fabric script that creates and distributes an archive to web servers using the deploy function
+Fabric script that creates and distributes an archive to web
+servers using the deploy function
 """
 
 from fabric import task
@@ -8,8 +9,8 @@ from fabric.operations import local
 from datetime import datetime
 import os
 
-
-env.hosts = ['<IP web-01>', '<IP web-02>']  # Replace with your web server IP addresses
+# Replace with your web server IP addresses
+env.hosts = ['<IP web-01>', '<IP web-02>']
 
 
 @task
@@ -58,22 +59,26 @@ def do_deploy(c, archive_path):
         c.run('mkdir -p /data/web_static/releases/{}/'.format(archive_name))
 
         # Extract the contents of the archive to the target directory
-        c.run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(archive_filename, archive_name))
+        c.run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+              .format(archive_filename, archive_name))
 
         # Delete the archive from the web server
         c.run('rm /tmp/{}'.format(archive_filename))
 
         # Move the extracted contents to the parent directory
-        c.run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(archive_name, archive_name))
+        c.run('mv /data/web_static/releases/{}/web_static/* /data/web_static/\
+                releases/{}/'.format(archive_name, archive_name))
 
         # Remove the empty web_static directory
-        c.run('rm -rf /data/web_static/releases/{}/web_static'.format(archive_name))
+        c.run('rm -rf /data/web_static/releases/{}/web_static'
+              .format(archive_name))
 
         # Delete the symbolic link /data/web_static/current
         c.run('rm -rf /data/web_static/current')
 
         # Create a new symbolic link to the new version of the code
-        c.run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(archive_name))
+        c.run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+              .format(archive_name))
 
         return True
     except Exception as e:
