5043a4a1abe19c80bb702aff6c75fae434de8d4c
Henzz <khenok53@gmail.com>
2024-04-20 22:34:11 +0300

    task 5 html update and task 6 done web fram falsk

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application with 0.0.0.0, port 5000
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def hello_world():
+    """
+    '/' route page
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", methods=['GET'], strict_slashes=False)
+def hbnb():
+    """
+    '/hbnb' route page
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", methods=['GET'], strict_slashes=False)
+def c(text):
+    """
+    '/c/<text>' route page
+    """
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+def pythonParam(text='is cool'):
+    """
+    '/python/<text>' route page
+    """
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    """
+    '/number/<n>' route page
+    """
+    return "{} is a number".format(n)
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """
+    '/number_template/<n>' route page
+    """
+    return render_template('5-number.html', num=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_even(n):
+    """
+    '/number_odd_or_even/<n>' route page
+    """
+    text = 'even'
+    if n % 2 != 0:
+        text = 'odd'
+    return render_template('6-number_odd_or_even.html', num=n, txt=text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
