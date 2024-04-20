c52fd630c18ec67566d7385af4e60ab433ff43be
Henzz <khenok53@gmail.com>
2024-04-20 11:07:04 +0300

    task 1 pycodestyle fix web fram flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,12 +13,14 @@ def hello_world():
     """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", methods=['GET'], strict_slashes=False)
 def hello_world():
     """
     '/hbnb' route page
     """
     return "HBNB!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
