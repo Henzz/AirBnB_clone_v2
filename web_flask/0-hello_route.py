357622660ad15d75e7a5e7e55bbcb68d914a53c7
Henzz <khenok53@gmail.com>
2024-04-20 10:59:06 +0300

    task 0 syntax error fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,8 +6,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", 'GET', '', False)
-def hello_work():
+@app.route("/", methods=['GET'], strict_slashes=False)
+def hello_world():
     """
     '/' route page
     """
@@ -15,4 +15,4 @@ def hello_work():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000)
+    app.run(host='0.0.0.0', port=5000)
