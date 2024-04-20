2df917dbdff32ceb767e1744156d1cb5ceb15fe6
Henzz <khenok53@gmail.com>
2024-04-20 11:06:10 +0300

    task 1 done web framework flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application with 0.0.0.0, port 5000
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def hello_world():
+    """
+    '/' route page
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", methods=['GET'], strict_slashes=False)
+def hello_world():
+    """
+    '/hbnb' route page
+    """
+    return "HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
