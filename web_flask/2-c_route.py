896a3caa0a3fb5ee49d1940c73ceadbce6fc1e9c
Henzz <khenok53@gmail.com>
2024-04-20 11:28:34 +0300

    task 2 done web fram flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application with 0.0.0.0, port 5000
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def hello_world():
+    """
+    '/' route page
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", methods=['GET'], strict_slashes=False)
+def hbnb():
+    """
+    '/hbnb' route page
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", methods=['GET'], strict_slashes=False)
+def cParam(text=None):
+    """
+    '/c/<text>' route page
+    """
+    if text:
+        return "C"+' '+text
+    else:
+        return "C"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
